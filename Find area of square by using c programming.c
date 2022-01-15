f61f56cce645efd6cf336c2e86de49df96f55346
uzzalbhuiyan318 <21303179@iubat.edu>
2022-01-16 05:07:00 +0600

    area of square added

---
+++ Find area of square by using c programming.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int arm, area;
+    printf("Enter your arm height: ");
+    scanf("%d",&arm);
+    area=arm*arm;
+    printf("Area of square is %d",area);
+
+}
