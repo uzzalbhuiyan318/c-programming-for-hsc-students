dbff63864807b30d225548618f18a763f98a9fb2
uzzalbhuiyan318 <21303179@iubat.edu>
2022-01-23 23:49:15 +0600

    for loop added

---
+++ for loop problem here/print a word or sentence 10 time.c
@@ -0,0 +1,20 @@
+/*
+==========================================================================
+
+ Problem     : "I love programming" print this sentence 10 time with new line using for loop
+ Author      : Uzzal_Bhuiyan
+ Version     :
+ Copyright   : OpenSource
+
+==========================================================================
+*/
+
+#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("I love programming\n");
+    }
+}
